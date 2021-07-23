d89604e71a67fa8217668f87b4ae61ec100893a3
Robb Knight <desk@rknight.me>
2021-07-23 21:39:46 +0100

    Move calendar events to the same CLI

--- Sources/RemindersLibrary/CLI.swift
+++ Sources/RemindersLibrary/CLI.swift
@@ -2,6 +2,7 @@ import ArgumentParser
 import Foundation
 
 private let reminders = Reminders()
+private let calendarEvents = CalendarEvents()
 
 private struct ShowLists: ParsableCommand {
     static let configuration = CommandConfiguration(
@@ -52,7 +53,7 @@ private struct Anytime: ParsableCommand {
     }
 }
 
-private struct Add: ParsableCommand {
+private struct AddReminder: ParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "Add a reminder to a list")
 
@@ -99,17 +100,85 @@ private struct Complete: ParsableCommand {
     }
 }
 
+private struct Events: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Show upcoming events")
+
+    @Argument(
+        help: "The calendar to show events from")
+    var listName: String?
+
+    @Option(
+        name: .shortAndLong,
+        help: "The amount of days to show")
+    var limit: Int?
+
+    func run() {
+        calendarEvents.events(listName: self.listName, limit: self.limit)
+    }
+}
+
+private struct Calendars: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Show all calendars")
+
+    func run() {
+        calendarEvents.showCalendars()
+    }
+}
+
+private struct AddEvent: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Add a event to a calendar")
+
+    @Argument(
+        help: "The calendar to add to, see 'calendars' for names")
+    var listName: String
+
+    @Argument(
+        help: "The event title")
+    var event: [String]
+
+    @Option(
+        name: .shortAndLong,
+        help: "The date the event starts")
+    var startDate: String
+
+    @Option(
+        name: .shortAndLong,
+        help: "The date the event ends")
+    var endDate: String?
+
+    @Option(
+        name: .shortAndLong,
+        help: "The location of the event")
+    var location: String?
+
+    func run() {
+        calendarEvents.addEvent(
+            string: self.event.joined(separator: " "),
+            toListNamed: self.listName,
+            startDate: self.startDate,
+            endDate: self.endDate,
+            location: self.location
+        )
+    }
+}
+
 public struct CLI: ParsableCommand {
     public static let configuration = CommandConfiguration(
         commandName: "reminders",
         abstract: "Interact with macOS Reminders from the command line",
         subcommands: [
-            Add.self,
+            AddReminder.self,
             Complete.self,
             Show.self,
             ShowLists.self,
             Upcoming.self,
             Anytime.self,
+            Calendars.self,
+            Events.self,
+            AddEvent.self,
         ]
     )
 
---
+++ Sources/RemindersLibrary/CalendarEvents.swift
@@ -0,0 +1,103 @@
+import EventKit
+import Foundation
+
+private let Store = EKEventStore()
+private let dateFormatter = DateFormatter()
+
+private func formattedDueDateEvent(from date: Date) -> String? {
+    let dateformat = DateFormatter()
+    dateformat.dateFormat = "yyyy-MM-dd HH:mm"
+    return dateformat.string(from: date)
+}
+
+private func format(_ event: EKEvent) -> String {
+    let start = formattedDueDateEvent(from: event.startDate).map { "\($0)" } ?? ""
+    let end = formattedDueDateEvent(from: event.endDate).map { "\($0)" } ?? ""
+    var location = event.structuredLocation == nil ? "" : event.structuredLocation!.title!
+    location = location.replacingOccurrences(of: "\n", with: ", ")
+    return "{ \"id\": \"\(event.eventIdentifier!)\", \"title\": \"\(event.title ?? "?")\", \"start\": \"\(start)\", \"end\": \"\(end)\", \"allDay\": \"\(event.isAllDay)\", \"location\": \"\(location)\", \"calendar\": \"\(event.calendar.title)\", \"confirmed\": \"\(event.status == EKEventStatus.none || event.status == EKEventStatus.confirmed)\" }"
+}
+
+public final class CalendarEvents {
+    public static func requestAccess() -> Bool {
+        let semaphore = DispatchSemaphore(value: 0)
+        var grantedAccess = false
+        Store.requestAccess(to: .event) { granted, _ in
+            grantedAccess = granted
+            semaphore.signal()
+        }
+
+        semaphore.wait()
+        return grantedAccess
+    }
+
+    func showCalendars() {
+        let calendars = self.getCalendars()
+        for calendar in calendars {
+            print(calendar.title)
+        }
+    }
+
+    func events(listName: String?, limit: Int?) {
+        let calendar = listName != nil ? self.calendar(withName: listName!) : nil
+        let semaphore = DispatchSemaphore(value: 0)
+
+        let now = Date()
+
+        let days:TimeInterval = Double((limit == nil ? 5 : limit!)*24*3600)
+        let next = Date(timeIntervalSinceNow: +days)
+        let calendars = calendar != nil ? [calendar!] : []
+
+        let predicate = Store.predicateForEvents(withStart: now, end: next, calendars: calendars)
+
+        let events = Store.events(matching: predicate)
+
+        for event in events {
+            print(format(event))
+            semaphore.signal()
+        }
+
+        semaphore.wait()
+    }
+
+    func addEvent(string: String, toListNamed name: String, startDate: String, endDate: String?, location: String?) {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = "yyyy-MM-dd HH:mm"
+        let start = dateformat.date(from: startDate)
+        let end = endDate != nil ? dateformat.date(from: endDate!) : start!.addingTimeInterval(60 * 60)
+
+        let calendar = self.calendar(withName: name)
+        let event = EKEvent(eventStore: Store)
+
+        event.calendar = calendar
+        event.title = string
+        event.startDate = start
+        event.endDate = end
+        if (location != nil)
+        {
+            event.structuredLocation = EKStructuredLocation(title: location!)
+        }
+
+        do {
+            try Store.save(event, span: .thisEvent, commit: true)
+            print("Added '\(event.title!)' to '\(calendar.title)'")
+        } catch let error {
+            print("Failed to save event with error: \(error)")
+            exit(1)
+        }
+    }
+
+    private func calendar(withName name: String) -> EKCalendar {
+        if let calendar = self.getCalendars().find(where: { $0.title.lowercased() == name.lowercased() }) {
+            return calendar
+        } else {
+            print("No reminders list matching \(name)")
+            exit(1)
+        }
+    }
+
+    private func getCalendars() -> [EKCalendar] {
+        return Store.calendars(for: .event)
+                .filter { $0.allowsContentModifications }
+    }
+}
